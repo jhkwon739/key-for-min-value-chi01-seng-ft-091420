25af9edd7e67bc788f43564875531785028f9f71
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-15 23:44:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,10 @@ def key_for_min_value(name_hash)
   #get first hash key value pair
   min_key, min_value = hash.first 
   name_hash.each do |key, value|
-    if min_value < value 
-      min_v
+    if value < min_value 
+      min_value = value
+      min_key = key
+    end
   end
+  min_key
 end
