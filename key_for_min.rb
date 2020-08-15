eb05e9601659130c4a9b433f13de7ea99e21ca6b
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-15 23:43:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,10 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil 
   end
+  #get first hash key value pair
   min_key, min_value = hash.first 
   name_hash.each do |key, value|
+    if min_value < value 
+      min_v
   end
 end
